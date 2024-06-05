e6ab6f5c1ba2e12490a506c80307721d2cc1c482
Yogesh-Ginti <yogeshginti@gmail.com>
2024-06-05 17:11:43 +0530

    removed /user route

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const { handleSignUp, handleLogin, handleLogout } = require("../controllers/userControllers");
 
 
-router.get('/', (req, res)=>{
-  res.send("go ahead")
-})
+
 // Route to register
 router.post('/signup', handleSignUp);
 // Route to Login
