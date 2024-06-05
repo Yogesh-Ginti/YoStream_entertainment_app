bf32e2a77d342c18e58626ca38b5b6167c6dfd31
Yogesh-Ginti <yogeshginti@gmail.com>
2024-06-05 09:32:51 +0530

    add hardcoded fe_url for testing purpose

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ connectDB()
 
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "https://yo-stream-entertainment-api.vercel.app/", credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser())
