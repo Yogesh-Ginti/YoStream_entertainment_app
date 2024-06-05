e666153240072c7044b31752ec25d69b9862d7b0
Yogesh-Ginti <yogeshginti@gmail.com>
2024-06-05 17:00:18 +0530

    mm

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api' : "https://yo-stream-entertainment-app.vercel.app",
-      
-    }
+  server: {
+    proxy: {
+      // Configure your proxy rules here
+      '/api': { // Matches requests starting with '/api'
+        target: 'https://yo-stream-entertainment-app.vercel.app', // Replace with your backend API URL
+        changeOrigin: true, // Change the origin header to match the target server
+      },
+    },
   },
   plugins: [react()],
 })
